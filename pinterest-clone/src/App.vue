381a1b715921544ef0e3b159ffbfa4cc7376307a
Berkaycan Sakar <berkaycansakar@gmail.com>
2022-05-13 18:38:54 +0300

    finished

--- pinterest-clone/src/App.vue
+++ pinterest-clone/src/App.vue
@@ -1,28 +1,76 @@
 <template>
-<div>
-    <TopNav/>
-    <div class="px-20">
-        <div class="w-1/6">
-            <Card src="1.jpg"/>
+  <div>
+    <TopNav />
+    <div class="px-20 w-full flex flex-wrap">
+      <div v-for="arr in subArrays" :key="arr" class="w-1/5">
+        <div v-for="card in arr" :key="card" class="w-full">
+          <div class="w-full p-2">
+            <Card :src="card.src" class="w-full h-full" />
+            <div class="">
+              <p class="font-bold"></p>
+              <p></p>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-
-
+  </div>
 </template>
 
 <script>
-import TopNav from './TopNav.vue'
-import Card from './Card.vue'
+import TopNav from "./TopNav.vue";
+import Card from "./Card.vue";
 export default {
-components:{
+  components: {
     TopNav,
-    Card
-
-} 
-}
+    Card,
+  },
+  computed: {
+    subArrays() {
+      var length = Math.ceil(this.cards.length / 5);
+      const result = new Array(length)
+        .fill()
+        .map((i) => {
+          console.log(i)
+          return this.cards.splice(0, length)
+        })
+        return result
+    }
+  },
+  data() {
+    return {
+      cards: [
+        { src: "1.jpg" },
+        { src: "2.jpg" },
+        { src: "3.jpg" },
+        { src: "4.jpg" },
+        { src: "5.jpg" },
+        { src: "6.jpg" },
+        { src: "7.jpg" },
+        { src: "8.jpg" },
+        { src: "9.jpg" },
+        { src: "10.jpg" },
+        { src: "11.jpg" },
+        { src: "12.jpg" },
+        { src: "13.jpg" },
+        { src: "14.jpg" },
+        { src: "15.jpg" },
+        { src: "16.jpg" },
+        { src: "17.jpg" },
+        { src: "18.jpg" },
+        { src: "19.jpg" },
+        { src: "20.jpg" },
+        { src: "21.jpg" },
+        { src: "22.jpg" },
+        { src: "23.jpg" },
+        { src: "15.jpg" },
+        { src: "5.jpg" },
+       
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
--- pinterest-clone/src/Card.vue
+++ pinterest-clone/src/Card.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="relative">
+  <div @mouseover="hover = true" @mouseleave="hover = false" class="relative">
     <img :src="src" class="rounded-lg" />
     <div
+      v-if="hover"
       class="absolute bg-dark opacity-50 inset-0 w-full h-full rounded-lg"
     ></div>
-    <div class="absolute top-0 w-full h-full flex flex-col justify-between p-3">
+    <div
+      v-if="hover"
+      class="absolute top-0 w-full h-full flex flex-col justify-between p-3"
+    >
       <div class="flex items-center justify-between">
         <button type="button" class="flex items-center">
-          <p class="font-semibold text-white">UI</p>
+          <p class="font-semibold text-white">Title</p>
           <span class="material-icons text-white">expand_more</span>
         </button>
         <button
@@ -17,22 +21,67 @@
         </button>
       </div>
       <div class="flex items-center w-full">
-          <button type="button" class="ml-auto mr-2 bg-white w-8 h-8 flex items-center justify-center rounded-full p-2 text-dark opacity-80 hover:opacity-100">
-              <span class="material-icons text-base">ios_share</span>
-          </button>
-          <button type="button" class=" bg-white rounded-full w-8 h-8 flex items-center justify-center p-2 text-dark opacity-80 hover:opacity-100">
-              <span class="material-icons text-base">more_horiz</span>
-          </button>
+        <button
+          type="button"
+          class="
+            ml-auto
+            mr-2
+            bg-white
+            w-8
+            h-8
+            flex
+            items-center
+            justify-center
+            rounded-full
+            p-2
+            text-dark
+            opacity-80
+            hover:opacity-100
+          "
+        >
+          <span class="material-icons text-base">ios_share</span>
+        </button>
+        <button
+          type="button"
+          class="
+            bg-white
+            rounded-full
+            w-8
+            h-8
+            flex
+            items-center
+            justify-center
+            p-2
+            text-dark
+            opacity-80
+            hover:opacity-100
+          "
+        >
+          <span class="material-icons text-base">more_horiz</span>
+        </button>
       </div>
     </div>
   </div>
 </template>
  
+
 <script>
 export default {
   props: ["src"],
+  data() {
+    return {
+      hover: false,
+     
+    };
+  },
 };
 </script>
+
+
+
+
+
+
 
 <style>
 </style>
